bfa46e27f462fa88ce3b0f7fab62c9942d18efe3
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2016-09-01 13:26:54 -0400

    Move example data to "inst/extdata"

--- inst/shiny-apps/mctestanalysis/global.R
+++ inst/shiny-apps/mctestanalysis/global.R
@@ -1,11 +1,11 @@
 library(shiny)
 
-ak_file <- system.file("data", 'answer_key_example.csv', package = 'MCTestAnalysis')
+ak_file <- system.file("extdata", 'answer_key_example.csv', package = 'MCTestAnalysis')
 if (ak_file != '') {
   answer_key_example <- read.csv(ak_file, stringsAsFactors = FALSE)
 }
 
-test_file <- system.file("data", "test_example.csv", package = "MCTestAnalysis")
+test_file <- system.file("extdata", "test_example.csv", package = "MCTestAnalysis")
 if (test_file != '') {
   test_example <- read.csv(test_file, stringsAsFactors = FALSE)
 }
