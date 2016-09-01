f5c3b9bc42072e649fbb8d4de55efe0b60d61f6b
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2016-09-01 13:24:58 -0400

    Add functions to load data
    - loadAnswerKey
    - loadTestData
    - loadAllData

---
+++ R/mcTestAnalysisData.R
@@ -0,0 +1,87 @@
+#' MC Test Analysis Data
+#'
+#' Load test and answer key data with \code{\link{loadTestData}} and
+#' \code{\link{loadAnswerKey}}. Creates an MC Test Analysis data object.
+#'
+#' @section Answer Key Data Format:
+#' The Answer Key file should contain four columns in the following order:
+#' \enumerate{
+#'    \item Question number, name or identifier: Eg. \code{"Q1"}, \code{"1"}, etc.
+#'    \item The correct answer for the question: Eg. \code{"1"}, \code{"A"}, etc.
+#'    \item A descriptive title for the question: Eg. \code{"Taylor Function"},
+#'          \code{"Tensor Flow"}, etc.
+#'    \item An identifier for the concept group to which the question belongs:
+#'          Eg. \code{"Taylor Series"}, \code{"A"}, \code{"Concept 1"}, etc.
+#' }
+#'
+#' @section Test Data Format:
+#' The test results data should contain as rows each student's response, with
+#' each question assigned a column. If the test results data contains student
+#' identifiers, these identifiers should be included in the first column, prior
+#' to the test answers. The MC Test Analysis Tool assumes that the question
+#' columns are in the same order as reported in the answers data.
+#'
+#' @param mctd Existing mcTestAnalysis data object
+#' @name mcTestAnalysisData
+NULL
+
+#' @describeIn mcTestAnalysisData Reads test data from CSV or TSV file and applies basic preprocessing.
+#' @inheritParams mcTestAnalysisData
+#' @param test_file Path to the test results data file
+#' @param has_student_id Does the first column of the test data include a student identifier?
+#' @param ... Arguments passed to \code{\link{read.csv}}
+#' @export
+loadTestData <- function(mctd = NULL, test_file, has_student_id = TRUE, ...) {
+  if (is.null(mctd)) mctd <- list()
+  x <- read.csv(test_file, stringsAsFactors = FALSE, ...)
+
+  q_index <- ifelse(has_student_id, 2, 1)
+  if (has_student_id) {
+    colnames(x)[1] <- 'id'
+    x$id <- factor(x$id)
+  }
+  if ('AnswerKey' %in% names(mctd)) {
+    colnames(x)[q_index:ncol(x)] <- mctd$AnswerKey$Question
+  }
+  mctd[['Test']] <- x
+  return(mctd)
+}
+
+#' @describeIn mcTestAnalysisData Reads answer_key data from CSV or TSV file and applies basic preprocessing.
+#' @inheritParams mcTestAnalysisData
+#' @param answer_file Path to the answer key data file
+#' @param ... Arguments passed to \code{\link{read.csv}}
+#' @export
+loadAnswerKey <- function(mctd = NULL, answer_file, ...) {
+  if (is.null(mctd)) mctd <- list()
+  x <- read.csv(answer_file, stringsAsFactors = FALSE, ...)
+
+  expected_columns <- c('Question', 'Answer', 'Title', 'Concept')
+
+  if (ncol(x) > 4) {
+    warning("Input data contained more than four columns, using only first four.")
+    x <- x[, 1:4]
+  } else if (ncol(x) == 3) {
+    warning("Input data contained 3 columns, assuming all questions from same 'General' concept.")
+    x$Concept <- 'General'
+  }
+
+  colnames(x) <- expected_columns
+
+  # Missing concepts given "Missing" concept group
+  x[is.na(x$Concept), 'Concept'] <- "Missing"
+
+  mctd[['AnswerKey']] <- x
+  return(mctd)
+}
+
+
+#' @describeIn mcTestAnalysisData Read answer_key and test data from CSV or TSV files
+#' @export
+loadAllData <- function(answer_file = NULL,
+                        test_file = NULL, has_student_id = TRUE, ...) {
+  mctd <- NULL
+  if (!is.null(answer_file)) mctd <- loadAnswerKey(mctd, answer_file, ...)
+  if (!is.null(test_file))   mctd <- loadTestData(mctd, test_file, has_student_id, ...)
+  return(mctd)
+}
--- inst/shiny-apps/mctestanalysis/server.R
+++ inst/shiny-apps/mctestanalysis/server.R
@@ -8,68 +8,32 @@ library(shiny)
 shinyServer(function(input, output) {
 
   # Reactive data elements ----
-
-  d_answer_key <- reactive({
-    if (is.null(input$f_answer_key)) return(NULL)
-    x <- read.csv(input$f_answer_key$datapath,
-                  header=input$o_import_header,
-                  sep=input$o_import_sep,
-                  quote=input$o_import_quote,
-                  stringsAsFactors = FALSE)
-
-    expected_columns <- c('Question', 'Answer', 'Title', 'Concept')
-
-    if (ncol(x) > 4) {
-      x <- x[, 1:4]
-    } else if (ncol(x) == 3) {
-      x$Concept <- 'General'
-    }
-
-    colnames(x) <- expected_columns
-
-    # Missing concepts given "Missing" concept group
-    x[is.na(x$Concept), 'Concept'] <- "Missing"
-    return(x)
+  mctd <- reactive({
+    loadAllData(answer_file = input$f_answer_key$datapath,
+                test_file      = input$f_test$datapath,
+                has_student_id = input$o_import_has_student_id,
+                header         = input$o_import_header,
+                sep            = input$o_import_sep,
+                quote          = input$o_import_quote)
   })
 
-  d_test <- reactive({
-    if (is.null(input$f_test)) return(NULL)
-    x <- read.csv(input$f_test$datapath,
-                  header=input$o_import_header,
-                  sep=input$o_import_sep,
-                  quote=input$o_import_quote,
-                  stringsAsFactors = FALSE)
-    q_index <- ifelse(input$o_import_has_student_id, 2, 1)
-    if (input$o_import_has_student_id) {
-      # may need to process the test data if first col is ids
-      colnames(x)[1] <- 'id'
-      x$id <- factor(x$id)
-    }
-    if (!is.null(d_answer_key())) {
-      colnames(x)[q_index:ncol(x)] <- d_answer_key()$Question
-    }
-    return(x)
-  })
-
-  concepts <- reactive({
-    d_answer_key[, 3]
-  })
+  # concepts <- reactive({
+  #   d_answer_key[, 3]
+  # })
 
   # Import Data Outputs ----
 
   output$t_answer_key <- renderDataTable({
     if (is.null(input$f_answer_key)) return(NULL)
-    d_answer_key()
+    mctd()$AnswerKey
   })
 
   output$t_test <- renderDataTable({
     if (is.null(input$f_test)) return(NULL)
-    d_test()
+    mctd()$Test
   })
 
   output$t_data_check <- renderText({
-    dtest <- d_test()
-    dansw <- d_answer_key()
     summary_text <- c()
 
     add_to_output <- function(...) {
@@ -77,8 +41,8 @@ shinyServer(function(input, output) {
       summary_text <<- c(summary_text, element)
     }
 
-    is_loaded_answer_key <- !is.null(d_answer_key())
-    is_loaded_test <- !is.null(d_test())
+    is_loaded_answer_key <- 'AnswerKey' %in% names(mctd())
+    is_loaded_test <- "Test" %in% names(mctd())
     if (!is_loaded_answer_key) {
       add_to_output('Please upload answer key.')
     }
@@ -89,14 +53,16 @@ shinyServer(function(input, output) {
 
     if (is_loaded_answer_key & is_loaded_test) {
       add_to_output('Questions:')
-      add_to_output('  - In answer key:', length(dansw$Question))
-      add_to_output('  - In test data: ', ifelse(input$o_import_has_student_id, ncol(dtest[, -1]), ncol(dtest)))
+      add_to_output('  - In answer key:', length(mctd()$AnswerKey$Question))
+      add_to_output('  - In test data: ', ifelse(input$o_import_has_student_id,
+                                                 ncol(mctd()$Test[, -1]),
+                                                 ncol(mctd()$Test)))
       add_to_output('')
       add_to_output('Responses:')
-      add_to_output('  - Incomplete:', nrow(dtest[!complete.cases(dtest),]))
-      add_to_output('  - Total:', nrow(dtest))
+      add_to_output('  - Incomplete:', nrow(mctd()$Test[!complete.cases(mctd()$Test),]))
+      add_to_output('  - Total:', nrow(mctd()$Test))
       add_to_output('')
-      add_to_output('Concepts:', length(unique(dansw$Concept)))
+      add_to_output('Concepts:', length(unique(mctd()$AnswerKey$Concept)))
     }
 
     paste(summary_text, collapse = '\n')
