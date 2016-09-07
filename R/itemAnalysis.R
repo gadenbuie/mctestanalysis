abb034038eb81538581c08570c2b8b757fa830f0
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2016-09-07 11:34:24 -0400

    Finalize conventional discrimination index

--- R/itemAnalysis.R
+++ R/itemAnalysis.R
@@ -7,7 +7,7 @@
 #' @inheritParams mcTestAnalysisData
 #' @export
 addItemScore <- function(mctd) {
-  should_have(mctd, 'Test', 'AnswerKey')
+  should_have(mctd, 'Test', 'AnswerKey', 'Test.complete')
   x <- apply(mctd$Test.complete, 1,
              function(x) as.integer(x == mctd$AnswerKey$Answer))
   x <- as.data.frame(t(x))
@@ -19,43 +19,36 @@ addItemScore <- function(mctd) {
 #' Add item analysis to MC Test Data Object
 #'
 #' Runs \code{\link{item.exam}} from the \link{psychometric} package and adds
-#' item analysis results to \link{mcTestAnalysisData} as \item{item.analysis}.
+#' item analysis results to \link{mcTestAnalysisData} as \link{item.analysis}.
 #' @inheritParams mcTestAnalysisData
 #' @export
-addItemAnalysis <- function(mctd) {
-  should_have(mctd, 'Test', 'AnswerKey')
+addItemAnalysis <- function(mctd, ...) {
+  should_have(mctd, 'Test', 'AnswerKey', 'Test.complete')
   if (!("item.score" %in% names(mctd))) {
     mctd <- addItemScore(mctd)
   }
-  mctd[['item.analysis']] <- psychometric::item.exam(mctd$item.score)
+  mctd[['item.analysis']] <- psychometric::item.exam(mctd$item.score, ...)
   return(mctd)
 }
 
-#' Discrimination To Plot Data
+
+#' Calculate Conventional Item Discrimination Index
 #'
-#' From Alvaro: We are getting discrimination index with this function. This
-#' function will allow you to obtain the discrimination index at the selected
-#' percentile once the code runs.
+#' Calculates the conventional item discrimination index, based on complete test
+#' results only. Follows the formulation described in Lopez (1998), where item
+#' discrimination \eqn{d = p(UG) - p(LG)}. UG and LG are the upper and lower
+#' groups -- the upper and lower 27\% of students as ranked by their overall
+#' test scores -- and \eqn{p(UG)} and \eqn{p(LG)} are the proportion of correct
+#' answers in each group.
 #'
-#' @keywords internal
-discriminationDifficultyPlot <- function(mctd, percentile = 0.27) {
-  if (!('item.analysis' %in% names(mctd))) mctd <- addItemAnalysis(mctd)
-  n_items <- ncol(mctd$Test.complete)
-
-  disc <- discrimination_index(mctd)
-
-  x <- data.frame('difficulty' = mctd$item.analysis[, 4],
-                  'discrimination' = disc,
-                  'question' = names(disc))
-  ggplot(x, aes(x = difficulty, y = discrimination, label = question))+
-    geom_point()+
-    geom_text(nudge_x = -0.025)+
-    theme_minimal()+
-    labs(x = "Difficulty Index", y = 'Discrimination Index')+
-    xlim(0, 1)+
-    ylim(0, 1)
-}
-
+#' @references \emph{The Item Discrimination Index: Does it Work?} Tristan Lopez
+#'   A. Rasch Measurement Transactions, 1998, 12:1 p. 626.
+#'   \url{http://www.rasch.org/rmt/rmt121r.htm}
+#'
+#' @inheritParams mcTestAnalysisData
+#' @param percentile Percentile defining upper and lower groups (standard is
+#'   0.27)
+#' @export
 discriminationIndex <- function(mctd, percentile = 0.27) {
   if (!("item.score" %in% names(mctd))) mctd <- addItemScore(mctd)
   n_students <- nrow(mctd$Test.complete)
@@ -68,19 +61,80 @@ discriminationIndex <- function(mctd, percentile = 0.27) {
   # L = # of students in lower percentile
   scores <- rowSums(mctd$item.score)
   cutoff <- quantile(scores, c(percentile, 1 - percentile))
-  above <- which(scores >= cutoff[2])
-  below <- which(scores <= cutoff[1])
+  upper <- which(scores >= cutoff[2])
+  lower <- which(scores <= cutoff[1])
   item_discrimination <- c()
   for (i in 1:n_items) {
     item <- mctd$item.score[, i]
-    H <- item[above]
+    H <- item[upper]
     h <- sum(H)
     H <- length(H)
-    L <- item[below]
+    L <- item[lower]
     l <- sum(L)
     L <- length(L)
     item_discrimination <- c(item_discrimination, h/H - l/L)
   }
   names(item_discrimination) <- colnames(mctd$Test.complete)
-  return(item_discrimination)
+  mctd[['discrimination_index']] <- item_discrimination
+  return(mctd)
+}
+
+#' Discrimination To Plot Data
+#'
+#' From Alvaro: We are getting discrimination index with this function. This
+#' function will allow you to obtain the discrimination index at the selected
+#' percentile once the code runs.
+#'
+#' @keywords internal
+discriminationDifficultyPlot <- function(mctd,
+                                         type = 'conventional',
+                                         show_labels = TRUE,
+                                         hide_legend = TRUE,
+                                         show_guidelines = TRUE,
+                                         max_limits = 'max_x') {
+  if (!('item.analysis' %in% names(mctd))) mctd <- addItemAnalysis(mctd)
+  if (is.null(type)) type <- 'conventional'
+  if (type == 'conventional') {
+    if (!('discrimination_index' %in% names(mctd))) mctd <- discriminationIndex(mctd)
+    disc <- mctd$discrimination_index
+  } else if (type == 'pbcc') {
+    return(NULL) # pass for now
+  } else if (type == 'pbcc_modified') {
+    return(NULL) # pass for now
+  } else {
+    stop(paste('Unknown discrimination index type', type))
+  }
+  n_items <- ncol(mctd$Test.complete)
+
+  x <- data.frame('difficulty' = mctd$item.analysis[, 'Difficulty'],
+                  'discrimination' = disc,
+                  'question' = names(disc))
+  x$question <- factor(x$question, levels = colnames(mctd$Test.complete))
+  g <- ggplot(x, aes(x = difficulty, y = discrimination, label = question, color = question))+
+    geom_point()+
+    theme_minimal()+
+    labs(x = "Difficulty Index", y = 'Discrimination Index', color = 'Question')
+
+  if (show_labels) {
+    g <- g + geom_text(nudge_x = -0.025, show.legend = FALSE)
+  }
+  if (hide_legend) {
+    g <- g + guides(color = FALSE)
+  }
+  if (show_guidelines) {
+    g <- g +
+      geom_vline(xintercept = 0.2, linetype = 'dashed', color = 'grey80')+
+      geom_vline(xintercept = 0.8, linetype = 'dashed', color = 'grey80')+
+      geom_hline(yintercept = 0.2, linetype = 'dashed', color = 'grey80')
+  }
+  if (!is.null(max_limits)) {
+    g <- switch(max_limits,
+                'max_y' = g + ylim(-1, 1),
+                'max_y+' = g + ylim(0, 1),
+                'max_x' = g + xlim(0, 1),
+                'max_all' = g + ylim(-1, 1) + xlim(0, 1),
+                'max_all+' = g + ylim(0, 1) + xlim(0, 1)
+                )
+  }
+  return(g)
 }
