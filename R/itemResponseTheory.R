7f0dfb6321eee3f61df75761bf7925f391a65a36
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2016-10-27 11:45:03 -0400

    Better handling of concept group in tetrachoric plot

--- R/itemResponseTheory.R
+++ R/itemResponseTheory.R
@@ -39,13 +39,31 @@ addIRTfits <- function(mctd) {
 #' @export
 plotTetrachoric <- function(mctd, group_by_concept = TRUE, equal_coords = FALSE) {
   should_have(mctd, 'item.score', 'AnswerKey')
+
   if (group_by_concept) {
-    ak_ordered <- mctd$AnswerKey %>% mutate(n = 1:nrow(.)) %>% arrange(Concept, n)
+    concepts <- unique(mctd$AnswerKey$Concept)
+    already_grouped <- mctd$AnswerKey %>%
+      mutate(Concept.lag = lag(Concept),
+             Concept.lag = ifelse(is.na(Concept.lag), 'abcxyz', Concept.lag)) %>%
+      filter(Concept != Concept.lag) %>%
+      {.$Concept == concepts} %>%
+      all()
+    ak_ordered <- mctd$AnswerKey %>% mutate(n = 1:nrow(.))
+    if (!already_grouped) {
+      ak_ordered <- ak_ordered %>% arrange(Concept, n)
+      concepts <- unique(ak_ordered$Concept)
+    }
     question_order <- ak_ordered$Question
-    group_points <- ak_ordered %>% group_by(Concept) %>% summarize(x = max(n)) %>% .$x
+    ak_ordered <- ak_ordered %>% mutate(n = 1:nrow(.))
+    group_points <- c()
+    for (concept in concepts) {
+      max.this.concept <- max(ak_ordered[ak_ordered$Concept == concept, 'n'])
+      group_points <- c(group_points, max.this.concept)
+    }
   } else {
     question_order <- mctd$AnswerKey$Question
   }
+
   tetra <- psych::tetrachoric(mctd$item.score)$rho
   tetra <- tetra %>%
     as.data.frame() %>%
@@ -53,21 +71,25 @@ plotTetrachoric <- function(mctd, group_by_concept = TRUE, equal_coords = FALSE)
     reshape2::melt(id.vars = 'Question', variable.name = 'Question2', value.name = 'rho')
   tetra$Question <- factor(tetra$Question, levels = question_order)
   tetra$Question2 <- factor(tetra$Question2, levels = rev(question_order))
+
   g <- ggplot()+
     geom_raster(data = tetra, aes(x = Question, y = Question2, fill = rho))+
     labs(x = '', y = '', fill = '')+
     theme_minimal()+
-    scale_fill_gradient2(limits = c(-1, 1))
+    scale_fill_gradient2(limits = c(-1, 1))+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 
   if (group_by_concept) {
-    N.concept <- length(unique(mctd$AnswerKey$Concept))
+    N.concept  <- length(unique(mctd$AnswerKey$Concept))
+    N.question <- length(unique(mctd$AnswerKey$Question))
     g <- g +
-      geom_vline(xintercept = group_points[-N.concept]+0.5, alpha = 0.5) +
-      geom_hline(yintercept = 26-group_points[-N.concept]+0.5, alpha = 0.5) +
+      geom_vline(xintercept = group_points[-N.concept] + 0.5, alpha = 0.3) +
+      geom_hline(yintercept = N.question - group_points[-N.concept] + 0.5, alpha = 0.3) +
       geom_text(data = data.frame('Concept' = unique(mctd$AnswerKey$Concept),
-                                  'y' = 26-group_points+1,
+                                  'y' = N.question - group_points + 1,
                                   'x' = 1),
-                aes(label = Concept, y = y, x = x))
+                aes(label = Concept, y = y, x = x),
+                hjust = 0)
   }
   if (equal_coords) g <- g + coord_equal()
   g
