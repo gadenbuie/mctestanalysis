0975c8beacadfa72472ec00d8798248b0aa044a0
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2016-11-22 14:31:58 -0500

    Create explore() and report() aliases to shiny app/gadgets

--- R/run_app.R
+++ R/run_app.R
@@ -3,7 +3,7 @@
 #' Runs the MC Test ANalysis Shiny app.
 #'
 #' @export
-run_app <- function() {
+explore <- function() {
   appDir <- system.file("shiny-apps", "mctestanalysis", package = "MCTestAnalysis")
   if (appDir == "") {
     stop("Could not find shiny-apps directory. Try re-installing the MCTestAnalysis package.")
